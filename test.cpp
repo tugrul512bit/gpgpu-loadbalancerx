29f30eb8d77637db3d4009d63ed71ae123be9b78
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2022-02-21 18:29:07 +0300

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,44 @@
+//============================================================================
+// Name        : test.cpp
+// Author      : Tugrul
+// Description : tests for load balancing between compute devices
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+#include "LoadBalancerX.h"
+int main() {
+
+	std::vector<std::string> output(20);
+
+	LoadBalanceLib::LoadBalancerX<int> lb;
+
+
+	for(int i=0;i<20;i++)
+	{
+		lb.addWork(LoadBalanceLib::GrainOfWork<int>([&,i](int gpu){
+
+			// simulating different GPUs (high-end = less sleep)
+			std::this_thread::sleep_for(std::chrono::milliseconds(gpu+3));
+			output[i]=std::string("work:")+std::to_string(i)+std::string(" gpu:")+std::to_string(gpu);
+		}));
+	}
+	lb.addDevice(LoadBalanceLib::ComputeDevice<int>(0));
+	lb.addDevice(LoadBalanceLib::ComputeDevice<int>(1));
+	lb.addDevice(LoadBalanceLib::ComputeDevice<int>(2));
+	lb.addDevice(LoadBalanceLib::ComputeDevice<int>(3));
+	lb.addDevice(LoadBalanceLib::ComputeDevice<int>(4));
+	lb.addDevice(LoadBalanceLib::ComputeDevice<int>(5));
+
+	for(int i=0;i<10;i++)
+	{
+		lb.run();
+	}
+
+	for(int i=0;i<output.size();i++)
+	{
+		std::cout<<output[i]<<std::endl;
+	}
+	return 0;
+}
