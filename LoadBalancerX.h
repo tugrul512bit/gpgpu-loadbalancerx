7b4dd307f83f3339554495b7b9f8a45d7b4e10de
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2022-02-22 15:41:13 +0300

    Added dedicated thread for each device controller
    todo: pin threads to their cores

--- LoadBalancerX.h
+++ LoadBalancerX.h
@@ -12,6 +12,8 @@
 #include<thread>
 #include<vector>
 #include<chrono>
+#include<memory>
+#include<mutex>
 
 
 namespace LoadBalanceLib
@@ -46,7 +48,7 @@ namespace LoadBalanceLib
 	public:
 		GrainOfWork(){ work=[](State state){ std::this_thread::sleep_for(std::chrono::seconds(1)); }; }
 		GrainOfWork(std::function<void(State)> workPrm){ work=workPrm; }
-		void run(State state){ work(state);}
+		void run(State state){ if(work) work(state);}
 	private:
 		std::function<void(State)> work;
 	};
@@ -71,34 +73,159 @@ namespace LoadBalanceLib
 	class LoadBalancerX
 	{
 	public:
-		LoadBalancerX()
+		LoadBalancerX():mutGlobal(std::make_shared<std::mutex>()),initialized(false)
 		{
 
 		}
 
-		void addWork(GrainOfWork<State> work){ totalWork.push_back(work); }
-		void addDevice(ComputeDevice<State> devPrm){ devices.push_back(devPrm); }
+		~LoadBalancerX()
+		{
+			for(size_t i=0; i<thr.size(); i++)
+			{
+				{
+					std::lock_guard<std::mutex> lg(*(mutGlobal));
+					running[i]=false;
+					initialized=true;
+				}
+
+				{
+
+					std::lock_guard<std::mutex> lg(*(mut[i]));
+					running[i]=false;
+					initialized=true;
+				}
+
+			}
+
+			for(size_t i=0; i<thr.size(); i++)
+			{
+				if(thr[i].joinable())
+					thr[i].join();
+			}
+		}
+
+		void addWork(GrainOfWork<State> work)
+		{
+			std::lock_guard<std::mutex> lg(*(mutGlobal));
+			totalWork.push_back(work);
+		}
+		void addDevice(ComputeDevice<State> devPrm)
+		{
+			size_t indexThr;
+			{
+				std::lock_guard<std::mutex> lg(*(mutGlobal));
+				initialized=false;
+
+				indexThr = thr.size();
+
+				mut.push_back(std::make_shared<std::mutex>());
+
+				{
+					std::lock_guard<std::mutex> lg(*(mut[indexThr]));
+					devices.push_back(devPrm);
+					running.push_back(true);
+					hasWork.push_back(false);
+					workComplete.push_back(true);
+
+					performances.push_back(1.0);
+					nsDev.push_back(1);
+					grainDev.push_back(1);
+					startDev.push_back(0);
+				}
+
+			}
+
+			thr.push_back(std::thread([&,indexThr](){
+
+				State state;
+				{
+					std::lock_guard<std::mutex> lg(*(mut[indexThr]));
+					state = devices[indexThr].getState();
+				}
+				bool isRunning = true;
+				bool hasWrk = false;
+				bool init=false;
+				size_t start = 0;
+				size_t grain = 0;
+
+
+				while(!init)
+				{
+					{
+						std::lock_guard<std::mutex> lg(*(mutGlobal));
+						init=initialized;
+					}
+				}
+
+				bool globalSyncDone = false;
+				while(isRunning)
+				{
+
+					{
+						if(globalSyncDone)
+						{
+							std::lock_guard<std::mutex> lg(*(mut[indexThr]));
+							isRunning = running[indexThr];
+							hasWrk = hasWork[indexThr];
+							start = startDev[indexThr];
+							grain = grainDev[indexThr];
+						}
+						else
+						{
+							std::lock_guard<std::mutex> lg(*(mutGlobal));
+							globalSyncDone=true;
+							isRunning = running[indexThr];
+							hasWrk = hasWork[indexThr];
+							start = startDev[indexThr];
+							grain = grainDev[indexThr];
+						}
+
+					}
+
+					if(hasWrk)
+					{
+						// compute grain
+						size_t elapsedDevice;
+						{
+							Bench benchDevice(&elapsedDevice);
+							if(grain>0)
+							{
+								const size_t first = start;
+								const size_t last = first+grain;
+								for(size_t j=first; j<last; j++)
+								{
+									totalWork[j].run(state);
+								}
+							}
+						}
+
+						{
+							std::lock_guard<std::mutex> lg(*(mut[indexThr]));
+							workComplete[indexThr]=true;
+							nsDev[indexThr]=elapsedDevice;
+						}
+
+					}
+
+				}
+			}));
+		}
+
 		void run()
 		{
+			{
+				std::lock_guard<std::mutex> lg(*(mutGlobal));
+				initialized=true;
+			}
 			const size_t totWrk = totalWork.size();
 			const size_t totDev = devices.size();
-
-			// initial guess for gpu performances
-			while(performances.size()<totDev)
-			{
-				performances.push_back(1.0f/totDev);
-				nsDev.push_back(1);
-				grainDev.push_back(1);
-
-				startDev.push_back(0);
-			}
 
 
 			// compute real performance
 			double totPerf = 0;
 			for(size_t i=0;i<totDev;i++)
 			{
-				double perf = grainDev[i]/(double)nsDev[i];
+				double perf = (grainDev[i]+0.1)/(double)nsDev[i];
 				totPerf+=perf;
 				performances[i]=perf;
 			}
@@ -106,18 +233,25 @@ namespace LoadBalanceLib
 			size_t ct=0;
 			for(size_t i=0;i<totDev;i++)
 			{
+				std::lock_guard<std::mutex> lg(*(mut[i]));
 				performances[i]/=totPerf;
 				grainDev[i]=performances[i]*totWrk;
 				ct+=grainDev[i];
 			}
 
 			// if all devices have 0 work or num work < num device
-			if(ct < totWrk)
-				grainDev[0]+=totWrk-ct;
+			size_t ctct=0;
+			while(ct < totWrk)
+			{
+				std::lock_guard<std::mutex> lg(*(mut[ctct%totDev]));
+				grainDev[ctct%totDev]++;
+				ct++;ctct++;
+			}
 
 			ct=0;
 			for(size_t i=0;i<totDev;i++)
 			{
+				std::lock_guard<std::mutex> lg(*(mut[i]));
 				startDev[i]=ct;
 				ct+=grainDev[i];
 
@@ -131,49 +265,28 @@ namespace LoadBalanceLib
 
 
 
-			for(auto & n:nsDev)
-			{
-				n=0;
-			}
-
-
-
 			size_t elapsedTotal;
 			{
 				Bench bench(&elapsedTotal);
 
 				// parallel run for real work & time measurement
-				std::vector<std::thread> thr;
 				for(size_t i=0; i<totDev; i++)
 				{
 					// todo: optimize with dedicated threads
-					thr.push_back(std::thread([&,i](){
-						size_t elapsedDevice;
-						if(grainDev[i]>0)
-						{
-							Bench benchDevice(&elapsedDevice);
-							const size_t first = startDev[i];
-							const size_t last = first+grainDev[i];
-							for(size_t j=first; j<last; j++)
-							{
-
-								totalWork[j].run(devices[i].getState());
-							}
-						}
-						else
-						{
-							elapsedDevice=-1; // max size_t nanoseconds
-						}
-						nsDev[i]=elapsedDevice;
-					}));
-
-
+					std::lock_guard<std::mutex> lg(*(mut[i]));
+					hasWork[i]=true;
+					workComplete[i]=false;
 				}
 
 				for(size_t i=0; i<totDev; i++)
 				{
-					if(thr[i].joinable())
-						thr[i].join();
+					bool finish = false;
+					while(!finish)
+					{
+						std::lock_guard<std::mutex> lg(*(mut[i]));
+						finish = workComplete[i];
+						std::this_thread::yield();
+					}
 				}
 			}
 			ns.push_back(elapsedTotal);
@@ -187,19 +300,27 @@ namespace LoadBalanceLib
 			size_t sz=performances.size();
 			for(size_t i=0;i<sz;i++)
 			{
+				std::lock_guard<std::mutex> lg(*(mut[i]));
 				result.push_back(performances[i]*100.0);
 			}
 			return result;
 		}
 	private:
+
 		std::vector<ComputeDevice<State>> devices;
 		std::vector<GrainOfWork<State>> totalWork;
 		std::vector<size_t> ns;
 		std::vector<size_t> nsDev;
 		std::vector<size_t> grainDev;
 		std::vector<size_t> startDev;
-
+		std::vector<std::thread> thr;
 		std::vector<double> performances;
+		std::vector<std::shared_ptr<std::mutex>> mut;
+		std::vector<bool> running;
+		std::vector<bool> hasWork;
+		std::vector<bool> workComplete;
+		std::shared_ptr<std::mutex> mutGlobal;
+		bool initialized;
 	};
 
 }
